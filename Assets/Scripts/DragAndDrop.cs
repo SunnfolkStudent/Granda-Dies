7f490319d0c3c70f6a99458796e56b8bcf73cdc2
Elva03 <146328146+Elva03@users.noreply.github.com>
2024-01-27 09:55:30 +0100

    Update DragAndDrop.cs

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -7,14 +7,20 @@ using UnityEngine.EventSystems;
 public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
     private Transform _transform;
+    private Vector3 _dragOffset;
+    private Camera _camera;
+    [SerializeField] private float _speed = 100;
     private void Awake()
     {
         _transform = GetComponent<Transform>();
+        _camera = Camera.main;
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         Debug.Log("OnPointerDown");
+        _dragOffset = transform.position - GetMousePos();
+
     }
 
     public void OnBeginDrag(PointerEventData eventData)
@@ -30,12 +36,12 @@ public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler
     public void OnDrag(PointerEventData eventData)
     {
         Debug.Log("OnDrag");
-        _transform.position = GetMousePos();
+        _transform.position = Vector3.MoveTowards(transform.position, GetMousePos() + _dragOffset, _speed * Time.deltaTime);
     }
 
     Vector3 GetMousePos()
     {
-        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var mousePos = _camera.ScreenToWorldPoint(Input.mousePosition);
         mousePos.z = 0;
         return mousePos;
     }
