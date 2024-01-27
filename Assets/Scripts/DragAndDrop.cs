e88cd2528fabe3c9613a55eb2bbaf963fa505a1d
Elva03 <146328146+Elva03@users.noreply.github.com>
2024-01-27 10:27:05 +0100

    Drag and drop

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -9,18 +9,20 @@ public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler
     private Transform _transform;
     private Vector3 _dragOffset;
     private Camera _camera;
+    private Vector3 _resetPos;
+    [SerializeField] private GameObject _dropSlot;
     [SerializeField] private float _speed = 100;
     private void Awake()
     {
         _transform = GetComponent<Transform>();
         _camera = Camera.main;
+        _resetPos = this.transform.localPosition;
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         Debug.Log("OnPointerDown");
         _dragOffset = transform.position - GetMousePos();
-
     }
 
     public void OnBeginDrag(PointerEventData eventData)
@@ -31,6 +33,16 @@ public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler
     public void OnEndDrag(PointerEventData eventData)
     {
         Debug.Log("OnEndDrag");
+        if (Mathf.Abs(this.transform.localPosition.x - _dropSlot.transform.localPosition.x) <= 0.5f 
+            && Mathf.Abs(this.transform.localPosition.y - _dropSlot.transform.localPosition.y) <= 0.5f)
+        {
+            Debug.Log("Correct area");
+            //this.transform.localPosition = new Vector3(_dropSlot.transform.localPosition.x, _dropSlot.transform.localPosition.y);
+        }
+        else
+        {
+            this.transform.localPosition = new Vector3(_resetPos.x, _resetPos.y, _resetPos.z);
+        }
     }
 
     public void OnDrag(PointerEventData eventData)
