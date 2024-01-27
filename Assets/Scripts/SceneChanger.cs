539b631ba6107631b578ede9757dd158ccdf8669
atBork <birknord3@gmail.com>
2024-01-27 12:19:14 +0100

    stufff

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour{
-
-    public void PlayGame ()
-    {
-        SceneManager.LoadSceneAsync(1);
-    }
+public class SceneChanger : MonoBehaviour
+{
+    
+public void ChangeScene (string sceneName)
+{
+    SceneManager.LoadScene (name);
+}
 
     public void QuitGame()
     {
         Application.Quit();
     }
+    
 }
