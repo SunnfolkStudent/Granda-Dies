aa2401231d358962b54009f07cb14de2977a3b87
1Polybius1 <146327892+1Polybius1@users.noreply.github.com>
2024-01-27 23:01:29 +0100

    Grandpa dying animation and reactive EKG

---
+++ Assets/Scripts/EKGManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EKGManager : MonoBehaviour
+{
+
+    [SerializeField] private Animator animator;
+    [SerializeField] private AudioClip flatlineSFX; 
+    private AudioSource audioSource;
+    private GrandpaManager grandpaManager;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponentInChildren<AudioSource>();
+        animator = GetComponent<Animator>();
+        grandpaManager = GameObject.FindWithTag("Grandpa").GetComponent<GrandpaManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (grandpaManager.grandpaHealth == 2)
+            animator.Play("Low");
+        else if (grandpaManager.grandpaHealth == 1)
+            animator.Play("Critical");
+        else if (grandpaManager.grandpaHealth == 0)
+            animator.Play("Chat is this real");
+            
+        if (grandpaManager.dead && !audioSource.clip.Equals(flatlineSFX))
+        {
+            audioSource.clip = flatlineSFX;
+            audioSource.Play();
+        }
+    }
+}
--- Assets/Scripts/GrandpaManager.cs
+++ Assets/Scripts/GrandpaManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,22 +7,54 @@ public class GrandpaManager : MonoBehaviour
 {
 
     private Animator animator;
+    public bool grandpaInfusedWithPiss, dead;
+    
+    private String currPlay;
+    public int grandpaHealth;
     
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        currPlay = "breathe no pee";
+        grandpaHealth = 3;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (grandpaHealth == 0 && !grandpaInfusedWithPiss)
+        {
+            dead = true;
+            GrandpaIsFuckingDead();
+        }
+        else if (grandpaHealth == 0 && grandpaInfusedWithPiss)
+        {
+            dead = true;
+            GrandpaIsFuckingDeadAndPissInfused();
+        }
     }
 
     public void InfuseWithPiss()
     {
-        animator.SetBool("isInfusedWithPiss", true);
+        animator.SetTrigger("aliveAndPissInfused");
+        grandpaInfusedWithPiss = true;
+        currPlay = "breathe pee";
+        return;
+    }
+    public void GrandpaIsFuckingDead()
+    {
+        animator.SetTrigger("deadNoPee");
+        dead = true;
+        currPlay = "still no pee";
+        return;
+    }
+    public void GrandpaIsFuckingDeadAndPissInfused()
+    {
+        animator.SetTrigger("deadAndPissInfused");
+        grandpaInfusedWithPiss = true;
+        dead = true;
+        currPlay = "still pee";
         return;
     }
 }
